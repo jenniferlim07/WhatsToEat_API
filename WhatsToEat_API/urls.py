c26d9c4c7505481d842db7c7683c7bb3941ec628
Jen Lim <jennifer.lim07@gmail.com>
2021-07-30 17:26:15 -0700

    change url to path

--- WhatsToEat_API/urls.py
+++ WhatsToEat_API/urls.py
@@ -13,14 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
+from django.contrib import admin
+from django.urls import path
+# from restaurants import urls
 from django.conf.urls import url, include 
 
 urlpatterns = [ 
+    path('admin/', admin.site.urls),
     url(r'^', include('restaurants.urls')),
+    # path('/', restaurants.urls)
 ]
--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from restaurants import views
+from django.urls import path
 
 urlpatterns = [
-    url(r'^restaurants$', views.restaurant_list),
-    url(r'^restaurants/(?P<pk>[0-9]+)$', views.restaurant_detail)
+    # url(r'^restaurants$', views.restaurant_list),
+    # url(r'^restaurants/(?P<pk>[0-9]+)$', views.restaurant_detail)
+    path('restaurants', views.restaurant_list),
+    path('restaurants/<int:pk>', views.restaurant_list)
+
 ]
