b10da8f32eb141c754734fcfef4cfc3d51be1379
Jen Lim <jennifer.lim07@gmail.com>
2021-08-16 15:04:15 -0700

    added delete cuisine

--- restaurants/views.py
+++ restaurants/views.py
@@ -20,24 +20,23 @@ class RestaurantViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         restaurants = Restaurant.objects.all()
-        print("testing", restaurants)
+        # print("testing", restaurants)
 
-        for restaurant in restaurants:
-            for cuisine in restaurant.cuisine.all():
-                print("r-c", cuisine.type)
+        # for restaurant in restaurants:
+        #     for cuisine in restaurant.cuisine.all():
+        #         print("r-c", cuisine.type)
 
         return restaurants
 
 
     def create(self, request, *arg, **kwargs):
         data = request.data
-        print("data ", data)
+        # print("data ", data)
         new_restaurant = Restaurant.objects.create(name=data["name"],website=data["website"], address=data["address"], city=data["city"],user_id=data["user"])
         new_restaurant.save()
 
         if data.get("cuisine"):
             # for cat in data["category"]:
-
             cuisine_object = Cuisine.objects.get(type=cuisine["type"])
             new_restaurant.cuisine.add(cuisine_object)
         
@@ -52,9 +51,9 @@ class CuisineViewSet(viewsets.ModelViewSet):
         cuisines = Cuisine.objects.all()
         print("get cuisines ", cuisines)
 
-        for cuisine in cuisines:
-            before = cuisine.restaurants.all()
-            print("before ", before)
+        # for cuisine in cuisines:
+        #     before = cuisine.restaurants.all()
+            # print("before ", before)
 
             # res = before.filter(restaurant=cuisine.restaurants["chinese"])
             # print("filtered ", res)
@@ -64,53 +63,37 @@ class CuisineViewSet(viewsets.ModelViewSet):
 
         return cuisines
 
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def restaurant_detail(request, pk):
-#     # find restaurant by pk (id)
-#     try:
-#         restaurant = Restaurant.objects.get(pk=pk)
-#     except Restaurant.DoesNotExist:
-#         return JsonResponse({'message': 'The restaurant does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    def create(self, request, *arg, **kwargs):
+        data = request.data
+        print("data ", data)
+        new_cuisine = Cuisine.objects.create(type=data["type"])
+        new_cuisine.save()
+        
+        serializer = CuisineSerializer(new_cuisine)
 
-#     # GET restaurant
-#     if request.method == 'GET':
-#         restaurants_serializer = RestaurantSerializer(restaurant)
-#         return JsonResponse(restaurants_serializer.data)
+        return Response(serializer.data)
 
-@api_view(['GET'])
+
+@api_view(['GET', 'DELETE'])
 def cuisine_restaurants(request, pk):
     cuisine = Cuisine.objects.get(pk=pk)
 
-    # for cuisine in cuisines:
-    #     print("before ", cuisine.restaurants.all())
-    #     for res in cuisine.restaurants.all():
-    #         print("cuisine res ", cuisine, res)
+    if request.method == 'GET':
+        cuisine_serializer = CuisineSerializer(cuisine)
 
-    # for cuisine in cuisines:
-    #     # res = Cuisine.objects.filter(restaurant=cuisine.restaurants["chinese"])
-    #     print("filtered ", res)
-    # print("cuisine pk ", cuisine)
+        restautant_list = []
+        for restaurant_id in cuisine_serializer.data["restaurants"]:
+            restaurant = Restaurant.objects.get(pk=restaurant_id)
+            restaurants_serializer = RestaurantSerializer(restaurant)
+            restautant_list.append(restaurants_serializer.data)
 
-    cuisine_serializer = CuisineSerializer(cuisine)
+        print("restaurant list ", restautant_list)
+        return JsonResponse(restautant_list, safe=False)
 
-    restautant_list = []
-    for restaurant_id in cuisine_serializer.data["restaurants"]:
-        # print("restaurant data", restaurant_id)
-        restaurant = Restaurant.objects.get(pk=restaurant_id)
-        # print("restaurant pk ", restaurant)
-        restaurants_serializer = RestaurantSerializer(restaurant)
-        # print("restaurant ser ", restaurants_serializer)
-        restautant_list.append(restaurants_serializer.data)
-
-    print("restaurant list ", restautant_list)
-
-
-    return JsonResponse(restautant_list, safe=False)
-
-
-    # cities = Restaurant.objects.order_by('city').distinct('city').filter(user=request.user)
-    # restaurants_serializer = RestaurantSerializer(cities, many=True)
-    # return JsonResponse(restaurants_serializer.data, safe=False)
+    # DELETE restaurant
+    elif request.method == 'DELETE':
+        cuisine.delete()
+        return JsonResponse({'message': 'Restaurant was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
 
 
 # Create your views here.
